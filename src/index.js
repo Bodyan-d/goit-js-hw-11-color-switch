891e7cb3015e5ab4b60a8ac582fa4971056be74d
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-05-20 20:08:08 +0300

    redy-first

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+let isSwitching = false;
+let intervalId = null;
+
+startBtnRef.addEventListener('click', onStartSwitchColors);
+stopBtnRef.addEventListener('click', onStopSwitchColors);
+
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+function onStartSwitchColors() {
+    if (!isSwitching) {
+        intervalId = setInterval(() => {
+            const randomColorIndex = randomIntegerFromInterval(0, colors.length - 1);
+            const randomColorItem = colors[randomColorIndex];
+
+            document.body.style.background = randomColorItem;
+            isSwitching = true
+        }, 1000);
+    };
+
+};
+
+function onStopSwitchColors() {
+    if (isSwitching) {
+        isSwitching = false;
+        clearInterval(intervalId);
+    }
+};
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
